6877d6a8e40872446d0e0867b9a4c6be244a8543
EgorFrom <31363936+EgorFrom@users.noreply.github.com>
2019-02-12 15:17:57 +0500

    Lesson_1-2
    Install webpack and crate

---
+++ code/js/reducers/car.js
@@ -0,0 +1,28 @@
+export default function () {
+	return [
+		{
+			id: 1,
+			car: "Audi",
+			speed: 234.45,
+			weight: 1.4,
+			desc: "R8 это замечательный спорткар!"
+			img: "https://autoreview.ru/images/Article/1661/Article_166188_860_575.jpg"
+		},
+		{
+			id: 2,
+			car: "BMW",
+			speed: 312.5,
+			weight: 1.2,
+			desc: "Автомобили с отметкой 'M' всегда были быстрее"
+			img: "https://www.bmw.ru/content/dam/bmw/common/all-models/3-series/sedan/2018/navigation/bmw-3-series-modellfinder.png"
+		},
+		{
+			id: 3,
+			car: "Mercedes",
+			speed: 286.75,
+			weight: 1.35,
+			desc: "Комфорт премиум класса!"
+			img: "https://icdn2.digitaltrends.com/image/2019-mercedes-amg-gt-4-door-coupe_35-1200x630-c-ar1.91.jpg"
+		}
+	]
+}
